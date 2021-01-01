ca8db9dd91eb482993744ade4e77a0cabdd21585
Karl Duggan <52557899+karlduggan@users.noreply.github.com>
2021-01-01 14:09:26 +0000

    Create Print_Object_Methods.py

---
+++ Print_Object_Methods.py
@@ -0,0 +1,14 @@
+# Snippets of code that are useful
+
+'''
+Very simply and useful tool for inspecting modules with very little documentation.
+Enter the object into the parameter and execute.
+'''
+
+def print_obj_methods(obj):
+    	object_methods = []
+    	for method_name in dir(obj):
+    		if callable(getattr(obj, method_name)):
+    			object_methods.append(method_name)
+    	for method_name in object_methods:
+    		print(method_name)
